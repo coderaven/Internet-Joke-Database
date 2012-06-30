11c474d054a727ccbf98ac0ef0d38d68f41b07b1
Raven G. Duran <me@imraven.com>
2012-06-30 17:43:24 +0800

    Added simple style and coded used php date

--- index.php
+++ index.php
@@ -3,8 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Internet Joke Database</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	Let's create a basic Internet Joke Database Website to learn PHP!
+	<div id="wrap">
+		<div id="content">
+			<p>
+				Let's create a basic Internet Joke Database Website to learn PHP!<br>
+				Today's date according to this webserver is: <strong><?php echo date('l, F js Y.'); ?></strong>
+			</p>
+		</div>
+	</div>
 </body>
 </html>
