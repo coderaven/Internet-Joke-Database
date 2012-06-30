fd23de4cdb2556a9bad96cd902991a67ee0081cf
Raven G. Duran <me@imraven.com>
2012-06-30 17:25:55 +0800

    Basic Site Structure and Initialization

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Internet Joke Database</title>
+</head>
+<body>
+	Let's create a basic Internet Joke Database Website to learn PHP!
+</body>
+</html>
