cc964901dac648bf002b6735d0c01451b485b3a7
Raven G. Duran <me@imraven.com>
2012-07-14 21:09:10 +0800

    Created include files header and footer

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>Internet Joke Database</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="wrap">
+		<div id="content">
--- index.php
+++ index.php
@@ -1,18 +1,8 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>Internet Joke Database</title>
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-	<div id="wrap">
-		<div id="content">
-			<p>
-				Let's create a basic Internet Joke Database Website to learn PHP!<br>
-				Today's date according to this webserver is: <strong><?php echo date('l, F js Y.'); ?></strong>
-			</p>
-		</div>
-	</div>
-</body>
-</html>
+<?php include "header.php" ?>
+
+<p>
+	Let's create a basic Internet Joke Database Website to learn PHP!<br>
+	Today's date according to this webserver is: <strong><?php echo date('l, F js Y.'); ?></strong>
+</p>
+
+<?php include "footer.php" ?>
